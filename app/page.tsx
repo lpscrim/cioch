3e7e7bec0731606661751f8ed18372c0051348ef
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-04-04 15:26:48 +0100

    Update global styles and modify layout metadata for Cioch Outdoor Clothing

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-[100vh] w-[100vw] overflow-hidden">
+    <div id="hero" className="h-[100vh] w-[100vw] overflow-hidden">
       <div className="relative h-full w-full">
         <Image
           src="/skye1.svg"
           alt="Skye"
-          className="z-100 "
+          className="-z-100 "
           priority
           layout="fill"
           objectFit="cover"
         />
+        <h1 className="absolute top-1/2 left-1/2 z-10 w-full max-w-[800px] -translate-x-1/2 -translate-y-1/2 text-center text-4xl font-bold text-black">
+          COICH
+        </h1>
       </div>
     </div>
   );
