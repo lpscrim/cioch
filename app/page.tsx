b199ab4f37c3efcadbdf29070baef952f47bbc7b
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-04-12 20:20:51 +0100

    Refactor hero section: update hero name ID and adjust scroll button for improved accessibility and consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
-import { ArrowDownIcon } from "@heroicons/react/20/solid";
-
 
 export default function Home() {
   return (
@@ -21,9 +19,9 @@ export default function Home() {
           <h3 className="text-2xl text-(--color-text) mb-2 fade-in-slow">Outdoor Clothing</h3>
           <h4 className="text-xl text-(--color-text-secondary) fade-in-slower">Handmade on the isle of Skye</h4>
         </div>
-        <button id="scrollButton" className="invisible md:visible absolute flex flex-row left-10 bottom-10 z-10 text-center text-(--color-text) text-md font-[700]">
+        <button id="scrollButton" className="invisible md:visible absolute flex flex-row font-mono left-15 bottom-15 z-10 text-center text-(--color-text) text-sm font-[700]">
           <p className="tracking-widest">SCROLL</p>
-          <ArrowDownIcon className="ml-2 size-4 mt-1 animate-bounce" />
+          <div className="ml-2 animate-bounce"><p className="rotate-90">➔</p></div>
         </button>
       </div>
     </div>
