3d779415adf5f265e1f1b429adf628a4e783e23a
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-04-12 20:11:17 +0100

    Refactor hero section: update text styles for consistency and improve scroll button accessibility

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+import { ArrowDownIcon } from "@heroicons/react/20/solid";
+
 
 export default function Home() {
   return (
@@ -15,14 +17,14 @@ export default function Home() {
           objectFit="cover"
         />
         <div id="heroText" className="absolute top-1/2 left-1/2 z-10 w-full max-w-[800px] -translate-x-1/2 -translate-y-1/2 text-center font-bold">
-          <h1 id="heroName" className={`mt-20 mb-2 text-9xl text-(--color-accent) fade-in tracking-wide`}>Cíoch</h1>
-          <h3 className="text-3xl text-(--color-text) mb-2 fade-in-slow">Outdoor Clothing</h3>
-          <h4 className="text-2xl text-(--color-text-secondary) fade-in-slower">Handmade on the isle of Skye</h4>
+          <h1 id="heroName" className={`mt-20 mb-2 text-8xl text-(--color-accent) fade-in tracking-wide`}>Cíoch</h1>
+          <h3 className="text-2xl text-(--color-text) mb-2 fade-in-slow">Outdoor Clothing</h3>
+          <h4 className="text-xl text-(--color-text-secondary) fade-in-slower">Handmade on the isle of Skye</h4>
         </div>
-        <div id="scrollButton" className="absolute left-10 bottom-10 z-10 text-center">
-          <span className="text-md font-[700] tracking-widest text-(--color-text) fade-in-slow">SCROLL</span>
-          <span className="ml-1 text-md font-extrabold animate-bounce">↓</span>
-        </div>
+        <button id="scrollButton" className="invisible md:visible absolute flex flex-row left-10 bottom-10 z-10 text-center text-(--color-text) text-md font-[700]">
+          <p className="tracking-widest">SCROLL</p>
+          <ArrowDownIcon className="ml-2 size-4 mt-1 animate-bounce" />
+        </button>
       </div>
     </div>
 
