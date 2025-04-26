9ed2aab5b6f0b21356a3d7e50a5114cc1ba83cfd
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-04-26 20:37:23 +0100

    Refactor code structure for improved readability and maintainability

---
+++ components/Card.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export default function Card({
+  title,
+  description,
+  imageSrc,
+  imageAlt,
+  color,
+}: {
+  title: string;
+  description: string;
+  imageSrc: string;
+  imageAlt: string;
+  color: string;
+}) {
+  return (
+    <div className={`card relative w-[200px] lg:w-[350px] bg-${color} rounded-lg shadow-md px-2 lg:px-15 py-30 lg:py-50`}>
+      <h2>Title: {title}</h2>
+      <p>Description: {description}</p>
+      <Image 
+        src={imageSrc} 
+        alt={imageAlt} 
+        width={150}
+        height={150}
+        className="-z-10 absolute top-0 left-0 rounded-lg w-full h-full" />
+    </div>
+  );
+}
--- components/CardScroller.tsx
+++ components/CardScroller.tsx
@@ -1,18 +1,73 @@
+import Card from "./Card";
 
 
+const cards = [
+    {
+        title: "Card 1",
+        description: "Description for Card 1",
+        imageSrc: "/jacket1.jpg",
+        imageAlt: "Image for Card 1",
+        color: "white",
+    },
+    {
+        title: "Card 2",
+        description: "Description for Card 2",
+        imageSrc: "/jacket2.jpg",
+        imageAlt: "Image for Card 2",
+        color: "amber-100",
+    },
+    {
+        title: "Card 3",
+        description: "Description for Card 3",
+        imageSrc: "/jacket3.jpg",
+        imageAlt: "Image for Card 3",
+        color: "amber-200",
+    },
+    {   title: "Card 4",
+        description: "Description for Card 4",
+        imageSrc: "/jacket4.jpg",
+        imageAlt: "Image for Card 4",
+        color: "amber-300",
+    },
+    {
+        title: "Card 5",
+        description: "Description for Card 5",
+        imageSrc: "/jacket5.jpg",
+        imageAlt: "Image for Card 5",
+        color: "amber-400",
+    },
+    {
+        title: "Card 6",
+        description: "Description for Card 6",
+        imageSrc: "/jacket1.jpg",
+        imageAlt: "Image for Card 6",
+        color: "amber-500",
+    },
+    {
+        title: "Card 7",
+        description: "Description for Card 7",
+        imageSrc: "/jacket1.jpg",
+        imageAlt: "Image for Card 7",
+        color: "white",
+    }
+]
 export default function CardScroller() {
     return (
-        <section className="py-15">
+        <section className="py-10">
             <div className="mx-auto w-full px-6">
                 <div className="flex justify-center overflow-x-hidden space-x-4 py-10">
-                    <div className="flex flex-nowrap space-x-8 side-to-side whitespace-nowrap pause">
-                        <div className="card min-w-[250px] lg:min-w-[500px] bg-white rounded-lg shadow-md px-15 py-30 lg:py-50">Card 1</div>
-                        <div className="card min-w-[250px] lg:min-w-[500px] bg-amber-100 rounded-lg shadow-md px-15 py-30 lg:py-50">Card 2</div>
-                        <div className="card min-w-[250px] lg:min-w-[500px] bg-amber-200 rounded-lg shadow-md px-15 py-30 lg:py-50">Card 3</div>
-                        <div className="card min-w-[250px] lg:min-w-[500px] bg-amber-300 rounded-lg shadow-md px-15 py-30 lg:py-50">Card 4</div>
-                        <div className="card min-w-[250px] lg:min-w-[500px] bg-amber-400 rounded-lg shadow-md px-15 py-30 lg:py-50">Card 5</div>
-                        <div className="card min-w-[250px] lg:min-w-[500px] bg-amber-500 rounded-lg shadow-md px-15 py-30 lg:py-50">Card 6</div>
-                        <div className="card min-w-[250px] lg:min-w-[500px] bg-white rounded-lg shadow-md px-15 py-30 lg:py-50">Card 7</div>
+                    <div className="flex flex-nowrap space-x-8 whitespace-nowrap slide">
+                        {cards.map((card, index) => (
+                            <div key={index}>
+                                <Card
+                                    title={card.title}
+                                    description={card.description}
+                                    imageSrc={card.imageSrc}
+                                    imageAlt={card.imageAlt}
+                                    color={card.color}
+                                />
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
