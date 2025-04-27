bdc71b4a9e4c42211bed02dca0234344cf9cd69f
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-04-27 18:53:35 +0100

    Update background opacity in PopoverPanel and DialogPanel for improved visibility

--- components/Header.tsx
+++ components/Header.tsx
@@ -66,7 +66,7 @@ export default function Example() {
             </PopoverButton>
             <PopoverPanel
               transition
-              className="absolute top-full -left-8 z-10 mt-3 w-screen max-w-[20rem] overflow-hidden rounded-xl bg-(--color-background)/80 ring-1 shadow-lg ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
+              className="absolute top-full -left-8 z-10 mt-3 w-screen max-w-[20rem] overflow-hidden rounded-xl bg-(--color-background)/85 ring-1 shadow-lg ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
             >
               <div className="p-4">
                 {categories.map((category) => (
@@ -100,7 +100,7 @@ export default function Example() {
       </nav>
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
         <div className="fixed inset-0 z-70" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-70 w-full overflow-y-auto bg-(--color-background)/90 text-(--color-text) px-6 py-6 sm:max-w-[20rem] sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-70 w-full overflow-y-auto bg-(--color-background)/95 text-(--color-text) px-6 py-6 sm:max-w-[20rem] sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <Link href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Cioch CLothing</span>
@@ -118,7 +118,7 @@ export default function Example() {
               className="-m-2.5 rounded-md p-2.5 "
             >
               <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="size-6" />
+              <XMarkIcon aria-hidden="true" className="size-6 cursor-pointer" />
             </button>
           </div>
           <div className="mt-6 flow-root">
