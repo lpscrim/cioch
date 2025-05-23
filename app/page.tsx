c1dfeb645dcb50fb18faf5cbe8a1fa02aed0fe18
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-05-23 20:26:57 +0100

    Hide scrollbar in global styles and adjust Hero component's container width for better layout control

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
   <div>
     <div className="absolute inset-0 bg-gradient-to-b from-black/50 via-transparent to-transparent z-2 bg-fixed"></div>
-    <div className="relative bg-[url(/storr.jpg)] bg-center bg-cover bg-no-repeat w-full  bg-fixed">
+    <div className="relative bg-[url(/storr.jpg)] bg-center bg-cover bg-no-repeat w-[100vw] overflow-hidden bg-fixed">
       <Hero />
     </div>
     <main>
