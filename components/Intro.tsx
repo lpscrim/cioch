732f5c5bc363bea5e09603397a2212d49a6c79d7
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-05-25 20:20:25 +0100

    Refactor Intro component layout and styling for improved consistency and readability

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -6,23 +6,23 @@ export default function Intro() {
       id="intro"
       className="w-full min-h-[90vh] h-full bg-foreground bg-[url(/mocha-grunge.png)]"
     >
-      <div className="flex flex-col max-w-[90rem] px-6 sm:px-12 mx-auto py-35">
-        <h2 className="text-4xl font-bold text-text-secondary sm:text-5xl text-center">
+      <div className="flex flex-col max-w-[90rem] px-6 sm:px-12 mx-auto py-25 lg:py-35">
+        <h2 className="text-4xl font-bold text-text-secondary sm:text-5xl text-center lg:mb-10">
           Welcome to <span id="cioch" className="text-secondary text-4xl sm:text-5xl font-[800]">Cíoch </span><span className="text-text-secondary">Outdoor Clothing</span>
         </h2>
-        <div className="flex flex-col lg:flex-row mx-auto pt-30">
-          <div className="w-full lg:w-1/2 mb-10 sm:mb-15 lg:mb-0 mx-auto flex flex-col items-center justify-center px-5">
+        <div className="flex flex-col lg:flex-row mx-auto pt-20">
+          <div className="w-full lg:w-1/2 mb-10 sm:mb-15 lg:mb-0 mx-auto flex flex-col items-center justify-start px-5">
             <Image
               src="/neilhelen.jpg"
               alt="Cioch Outdoor Clothing"
-              className="w-full max-w-[600px] h-auto rounded-lg"
+              className="w-full max-w-[500px] h-auto rounded-lg"
               width={360}
               height={450}
             />
           </div>
-          <div className="w-full lg:w-1/2 mx-auto flex flex-col items-center justify-start px-6">
+          <div className="w-full lg:w-1/2 mx-auto flex flex-col items-center justify-center px-6">
 
-            <p className="pt-5 ext-xl text-text-secondary sm:text-2xl mb-10 text-justify">
+            <p className="text-xl text-text-secondary sm:text-2xl text-justify">
               Neil and Helen Urquhart have been designing and manufacturing
               outdoor clothing for individual customers since 1988. From
               personal use, feedback from customers, professional mountaineers,
