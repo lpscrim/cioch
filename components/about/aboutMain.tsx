e46ab419b0aef82eff65a1d9c73d453abd62a637
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-06-09 17:47:10 +0100

    Update AboutMain component: adjust heading font size for improved readability

--- components/about/aboutMain.tsx
+++ components/about/aboutMain.tsx
@@ -2,7 +2,7 @@
 export default function AboutMain() { 
     return (
         <div>
-            <h2 className="text-4xl font-bold text-text-secondary sm:text-5xl text-center mt-10">
+        <h2 className="text-3xl font-bold text-text-secondary sm:text-4xl text-center mt-10">
           Cioch Outdoor Clothing
         </h2>
         <p className="mt-6 text-lg leading-8 text-text-secondary">
