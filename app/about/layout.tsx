fd945850da4b00649e6e856558535692c76e3364
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-06-16 20:55:33 +0100

    Update AboutLayout component: adjust width properties for improved layout consistency across different screen sizes

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -7,7 +7,7 @@ export default function AboutLayout({ children }: { children: React.ReactNode })
       className="flex items-start w-[100vw] min-h-[105vh] bg-foreground bg-[url(/brittle.jpg)] bg-cover bg-no-repeat bg-fixed relative"
     >
       <div className="absolute inset-0 bg-gradient-to-b from-black/30 via-transparent to-transparent bg-fixed pointer-events-none"></div>
-      <div className="min-h-[80vh] mb-5 pb-15 mt-25 flex flex-col md:max-w-[90vw] lg:max-w-[80vw] min-w-[80vw] px-6 pt-2 sm:px-12 mx-auto bg-foreground/80 rounded-md overflow-y-auto">
+      <div className="min-h-[80vh] mb-5 pb-15 mt-25 flex flex-col w-[90vw] md:w-[85vw] lg:w-[80vw] 2xl:w-[70vw] px-6 pt-2 sm:px-12 mx-auto bg-foreground/80 rounded-md overflow-y-auto">
         <AboutNav />
         {children}
         <h2 className="text-xl font-semibold text-text-secondary sm:text-2xl text-center mt-20 mx-auto">
