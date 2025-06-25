6f5869bcb0364b9afbeadb986fc015f01c958629
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-06-25 20:41:46 +0100

    Enhance Waterproof page layout: add header and description, update styling, and import Link component

--- app/shop/waterproof/page.tsx
+++ app/shop/waterproof/page.tsx
@@ -1,4 +1,5 @@
 import ProductCard from "@/components/shop/productCard";
+import Link from "next/link";
 
 const products = [
   {
@@ -64,14 +65,24 @@ const products = [
 ];
 
 export default function Waterproof() {
-
-
   return (
     <div className="px-6">
-       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="mx-auto max-w-2xl px-4 py-8 lg:max-w-7xl lg:px-8">
+        <div className="relative py-20 text-center bg-[url('/pic.jpg')] bg-cover bg-center bg-no-repeat rounded-lg">
+          <div className="absolute inset-0 bg-black/55 rounded-lg"></div>
+            <div className="relative z-10">  
+              <h1 className="text-4xl font-bold tracking-tight text-text">
+                Waterproofs
+              </h1>
+              <p className="mx-auto mt-6 max-w-3xl text-base text-text/90 font-bold">
+                Explore our range of waterproof <Link href='/about/nikwax'><span className="text-accent font-bold"> Nikwax </span> </Link>clothing designed to keep you dry and comfortable in any weather. We have everything you need for your outdoor adventures.
+              </p>
+            </div>  
+        </div>
+
         <h2 className="sr-only">Products</h2>
 
-        <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+        <div className="grid grid-cols-1 gap-y-4 py-8 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
           {products.map((product) => (
             <ProductCard product={product} key={product.id} />
           ))}
