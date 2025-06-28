db2ec0841625245cedd93011187cfa59421fbd10
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-06-28 16:30:53 +0100

    Update styling for ProductCard component and enhance layout in Windproof page

--- app/shop/windproof/page.tsx
+++ app/shop/windproof/page.tsx
@@ -40,7 +40,7 @@ export default function Windproof() {
         </div>
 
         <h2 className="sr-only">Products</h2>
-        <div className="grid grid-cols-1 gap-y-4 py-8 lg:grid-cols-2 sm:gap-x-6 sm:gap-y-10  lg:gap-x-8">
+        <div className="grid grid-cols-1 gap-y-4 py-8 lg:grid-cols-2 sm:gap-x-6 sm:gap-y-10  lg:gap-x-8 xl:px-12 lg:px-0 sm:px-8 px-0">
           {products.map((product) => (
             <ProductCard product={product} key={product.id} />
           ))}
--- components/shop/productCard.tsx
+++ components/shop/productCard.tsx
@@ -25,26 +25,26 @@ export default function ProductCard({
   return (
     <div
               key={id}
-              className="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
+              className="group relative flex flex-col overflow-hidden rounded-lg border border-text bg-text"
             >
               <Image
                 alt={imageAlt}
                 src={imageSrc}
                 width={500}
                 height={500}
-                className="aspect-3/4 w-full bg-gray-200 object-cover group-hover:opacity-75 sm:aspect-auto sm:h-96"
+                className="aspect-3/4 w-full bg-text object-cover group-hover:opacity-75 sm:aspect-auto sm:h-96"
               />
               <div className="flex flex-1 flex-col space-y-2 p-4">
-                <h3 className="text-sm font-medium text-gray-900">
+                <h3 className="text-sm font-medium text-text-secondary">
                   <a href={href}>
                     <span aria-hidden="true" className="absolute inset-0" />
                     {name}
                   </a>
                 </h3>
-                <p className="text-sm text-gray-500">{description}</p>
+                <p className="text-sm text-text-secondary">{description}</p>
                 <div className="flex flex-1 flex-col justify-end">
-                  <p className="text-sm text-gray-500 italic">{options}</p>
-                  <p className="text-base font-medium text-gray-900">{price}</p>
+                  <p className="text-sm text-text-secondary italic">{options}</p>
+                  <p className="text-base font-medium text-text-secondary">{price}</p>
                 </div>
               </div>
             </div>
