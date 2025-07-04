25e23a2a308748d9358d286fe940fc67277126da
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-07-04 18:58:37 +0100

    Refactor ImageViewer component: replace text arrows with icon buttons for improved visual consistency

--- components/gallery/ImageViewer.tsx
+++ components/gallery/ImageViewer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image, { StaticImageData } from "next/image";
+import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/20/solid";
 
 interface ImageViewerProps {
   selectedImage: StaticImageData;
@@ -26,16 +27,17 @@ const ImageViewer: React.FC<ImageViewerProps> = ({
         />
 
         <button
-          className="absolute top-1/2 transform -translate-y-1/2 left-0 text-white py-14 px-4 md:py-28 md:px-5 text-3xl md:text-5xl"
+          className="absolute top-1/2 transform -translate-y-1/2 left-0 text-text py-14 px-4 md:py-28 md:px-5 text-3xl md:text-5xl z-100"
           onClick={onPrev}
         >
-          &lt;
+          <ArrowLeftIcon className="w-10 h-10 text-text hover:text-secondary transition-colors cursor-pointer"  />
+          
         </button>
         <button
-          className="absolute top-1/2 transform -translate-y-1/2 right-0 text-white py-14 px-4 md:py-28 md:px-5 text-3xl md:text-5xl"
+          className="absolute top-1/2 transform -translate-y-1/2 right-0 text-text py-14 px-4 md:py-28 md:px-5 text-3xl md:text-5xl"
           onClick={onNext}
         >
-          &gt;
+          <ArrowRightIcon className="w-10 h-10 text-text hover:text-secondary transition-colors cursor-pointer"  />
         </button>
       </div>
     </div>
