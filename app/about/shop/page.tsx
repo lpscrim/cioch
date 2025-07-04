c6361d58ea01ea3c56c11614b9d3ba570ae30464
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-07-04 15:34:42 +0100

    Implement code changes to enhance functionality and improve performance

--- app/about/shop/page.tsx
+++ app/about/shop/page.tsx
@@ -1,30 +1,53 @@
 import Image from "next/image";
 export default function Shop() {
   return (
-    <div className="px-6 flex flex-col lg:flex-row mx-auto">
-      <div className="w-full lg:w-1/2 mx-auto flex flex-col items-center justify-start">
-        <h2 className="text-3xl font-bold text-secondary sm:text-4xl text-center my-10">
-          Our shop & Factory
-        </h2>
-        <p className="mt-6 text-lg sm:text-xl leading-8 text-text-secondary">
-          Opened in May 2000 our new shop gives customers the opportunity to
-          browse through our range of clothing and accessories under the shadow
-          of the Cuillin Hills. <br />
-          <br /> As well as our range of Outdoor Clothing, we also stock thermal
-          and leisure clothing from Paramo, Montane, Meindl boots, as well as
-          walking and climbing accessories and local books and maps. Please call
-          for further details.
-          <br />
-        </p>
+    <div className="px-6 flex flex-col border-4 mx-auto">
+      <div className="flex flex-col lg:flex-row">
+        <div className="w-full lg:w-1/2 mx-auto flex flex-col items-center justify-start">
+          <h2 className="text-3xl font-bold text-secondary sm:text-4xl text-center my-10">
+            Our shop & Factory
+          </h2>
+          <p className="mt-6 text-lg sm:text-xl leading-8 text-text-secondary">
+            Opened in May 2000 our new shop gives customers the opportunity to
+            browse through our range of clothing and accessories under the
+            shadow of the Cuillin Hills. <br />
+            <br /> As well as our range of Outdoor Clothing, we also stock
+            thermal and leisure clothing from Paramo, Montane, Meindl boots, as
+            well as walking and climbing accessories and local books and maps.
+            Please call for further details.
+            <br />
+          </p>
+        </div>
+        <div className="w-full lg:w-1/2 mt-15 mb-10 sm:mb-5 lg:mb-0 mx-auto flex flex-col items-center justify-center ">
+          <Image
+            src="/shop.jpg"
+            alt="Cioch Shop and Factory"
+            className="w-4/5 max-w-[400px] h-auto rounded-lg"
+            width={360}
+            height={450}
+          />
+        </div>
       </div>
-      <div className="w-full lg:w-1/2 mt-15 mb-10 sm:mb-5 lg:mb-0 mx-auto flex flex-col items-center justify-center ">
-        <Image
-          src="/shop.jpg"
-          alt="Cioch Shop and Factory"
-          className="w-4/5 max-w-[400px] h-auto rounded-lg"
-          width={360}
-          height={450}
-        />
+      <div className="flex flex-col lg:flex-row">
+        <div className="w-full lg:w-1/2 mx-auto flex flex-col items-center justify-start">
+          <h2 className="text-2xl font-bold text-secondary sm:text-3xl text-center my-10">
+            How to find us- From the Skye Bridge
+          </h2>
+          <p className="mt-4 text-lg sm:text-xl leading-8 text-text-secondary">
+            Follow the A87 to Sligachan, then take the A863 to Dunvegan. <br />
+            <br /> Struan is 14 miles from Sligachan.
+            <br />
+          </p>
+        </div>
+        <div className="w-full lg:w-1/2 mt-15 mb-10 sm:mb-5 lg:mb-0 mx-auto flex flex-col items-center justify-center ">
+          <Image
+            src="/map.gif"
+            alt="Directions on map of Skye"
+            className="w-4/5 max-w-[400px] h-auto rounded-lg"
+            width={360}
+            height={450}
+          />
+        </div>
       </div>
     </div>
   );
