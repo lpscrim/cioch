d5f7ee1ae9bbfbf197e265b72da0abb6170ebaa7
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-07-05 16:53:49 +0100

    removed privace policy from contact form

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -9,47 +9,38 @@ import {
   DialogTitle,
 } from "@headlessui/react";
 
-export default function Contact({open, setOpen}) {
+export default function Contact({ open, setOpen }: { open: boolean; setOpen: () => null; } ) {
   return (
     <Dialog open={open} onClose={setOpen} className="relative z-10">
       <DialogBackdrop
         transition
-        className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
+        className="fixed inset-0 bg-text-secondary/65 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
       />
 
-      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+      <div className="fixed inset-0 z-10 w-[100dvw] overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <DialogPanel
             transition
-            className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+            className="isolate py-30 lg:px-8 transform overflow-hidden  rounded-lg bg-text px-8 pt-5 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95"
           >
-            <div>
-              <div className="isolate bg-text px-6 py-24 sm:py-32 lg:px-8">
+
                 <div
                   aria-hidden="true"
                   className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
                 >
-                  <div
-                    style={{
-                      clipPath:
-                        "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-                    }}
-                    className="relative left-1/2 -z-10 aspect-1155/678 w-144.5 max-w-none -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-288.75"
-                  />
                 </div>
                 <div className="mx-auto max-w-2xl text-center">
-                  <h2 className="text-4xl font-semibold tracking-tight text-balance text-text-secondary sm:text-5xl">
-                    Contact sales
-                  </h2>
-                  <p className="mt-2 text-lg/8 text-text-secondary">
-                    Aute magna irure deserunt veniam aliqua magna enim
-                    voluptate.
+                  <DialogTitle className="text-4xl font-semibold tracking-tight text-balance text-text-secondary sm:text-5xl">
+                    Contact us
+                  </DialogTitle>
+                  <p className="mt-4 text-lg/8 text-text-secondary">
+                    Please send us an email with any questions you have!
                   </p>
                 </div>
                 <form
                   action="#"
                   method="POST"
-                  className="mx-auto mt-16 max-w-xl sm:mt-20"
+                  className="mx-auto mt-16 max-w-6xl sm:mt-20"
                 >
                   <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                     <div>
@@ -173,45 +164,18 @@ export default function Contact({open, setOpen}) {
                         />
                       </div>
                     </div>
-                    <div className="flex gap-x-4 sm:col-span-2">
-                      <div className="flex h-6 items-center">
-                        <div className="group relative inline-flex w-8 shrink-0 rounded-full bg-text-secondary/40 p-px inset-ring inset-ring-text-secondary/5 outline-offset-2 outline-accent transition-colors duration-200 ease-in-out has-checked:bg-accent has-focus-visible:outline-2">
-                          <span className="size-4 rounded-full bg-text shadow-xs ring-1 ring-text-secondary/5 transition-transform duration-200 ease-in-out group-has-checked:translate-x-3.5" />
-                          <input
-                            id="agree-to-policies"
-                            name="agree-to-policies"
-                            type="checkbox"
-                            aria-label="Agree to policies"
-                            className="absolute inset-0 appearance-none focus:outline-hidden"
-                          />
-                        </div>
-                      </div>
-                      <label
-                        htmlFor="agree-to-policies"
-                        className="text-sm/6 text-text-secondary"
-                      >
-                        By selecting this, you agree to our{" "}
-                        <a
-                          href="#"
-                          className="font-semibold textspace-nowrap text-accent"
-                        >
-                          privacy policy
-                        </a>
-                        .
-                      </label>
-                    </div>
                   </div>
                   <div className="mt-10">
                     <button
                       type="submit"
                       className="block w-full rounded-md bg-accent px-3.5 py-2.5 text-center text-sm font-semibold text-text shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent"
                     >
-                      Let&#39:s talk
+                      Let &#39; s talk
                     </button>
                   </div>
                 </form>
-              </div>
-            </div>
+
+
           </DialogPanel>
         </div>
       </div>
