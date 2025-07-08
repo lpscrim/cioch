5669dd12bc0d969d51a4797967391315fe946924
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-07-08 17:35:22 +0100

    made Hero background 100svh

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="relative min-h-[100lvh]">
-      <div className=" inset-0 -z-10 fixed h-[100lvh]">
+      <div className="inset-0 -z-10 fixed h-[100svh]">
           <div className="absolute inset-0 bg-gradient-to-b from-black/25 via-white/5 to-transparent z-2 bg-fixed h-[100lvh]"></div>
 
         <Image
           alt="123"
           src="/storr.webp"
           layout="fill"
-          className="w-auto h-full object-cover "
+          className="w-auto h-full object-cover"
           quality={100}
         />
       </div>
