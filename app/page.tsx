da24fbd00cc69d57ed6e990ad6c9d6a67089e0d2
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-07-08 11:12:14 +0100

    revert

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,11 @@ import "@/styles/home.css";
 export default function Home() {
   return (
   <div>
-    <div className="absolute inset-0 bg-gradient-to-b from-black/25 via-white/5 to-transparent z-2 bg-fixed h-[100lvh]"></div>
-    <div className="relative bg-[url(/storr.webp)] bg-center bg-cover bg-no-repeat w-[100lvw] overflow-hidden bg-fixed h-[100lvh]">
-      <div className="h-[100svh]">
-        <Hero />
-      </div>
+    <div className="absolute inset-0 bg-gradient-to-b from-black/25 via-white/5 to-transparent z-2 bg-fixed h-[100svh]"></div>
+    <div className="relative bg-[url(/storr.webp)] bg-center bg-cover bg-no-repeat w-[100lvw] overflow-hidden sm:bg-fixed h-[100svh]">
+      
+      <Hero />
+      
     </div>
     <main>
         <Intro />
