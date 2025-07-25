0d175361968ddfce83a016cb76e772b260910072
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-07-25 19:29:01 +0100

    Wrap NextStudio component in a div for improved layout structure

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -2,5 +2,9 @@ import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return (
+    <div className='z-9999'>
+      <NextStudio config={config} />
+    </div>
+  )
 }
