4a0cfc082123dd47d0742730903e7c2a55b6e902
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-09-12 20:34:18 +0100

    Adjust layout styles in ShopLayout for improved visual consistency

--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -10,10 +10,10 @@ export default function ShopLayout({
   return (
     <section
       id="shop"
-      className="flex items-start w-[100vw] min-h-[100lvh] bg-foreground  bg-cover bg-no-repeat bg-fixed relative"
+      className="flex items-start w-[100vw] min-h-[105%] bg-foreground relative"
     >
-      <div className="absolute inset-0 bg-gradient-to-b from-black/25 via-transparent to-transparent bg-fixed pointer-events-none z-0"></div>
-      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/25 bg-fixed pointer-events-none z-0"></div>
+      <div className="absolute inset-0 bg-gradient-to-b from-black/35 via-transparent to-transparent bg-fixed pointer-events-none z-0"></div>
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/30 bg-fixed pointer-events-none z-0"></div>
 
       <div className="min-h-[80vh] mt-20 sm:mt-25 flex flex-col w-[95vw] md:w-[90vw] lg:w-[80vw] xl:w-[70vw]  mx-auto overflow-y-auto">
         <div className="z-10">
