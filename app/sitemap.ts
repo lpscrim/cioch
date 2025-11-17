bda5335ab2288df6cc4340868650f97b84fe2cf4
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-11-17 13:51:27 +0000

    Fix URL for kids shop page in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -25,7 +25,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/shop/kids/kids`,
+      url: `${baseUrl}/shop/kids`,
       lastModified: new Date(),
       changeFrequency: 'monthly' as const,
       priority: 0.8,
