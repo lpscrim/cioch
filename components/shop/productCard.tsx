d5a8686171e0014281bb5b9dd9d8dce2a781fb1e
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-11-20 17:11:32 +0000

    Fix styling issues in ProductCard component for improved layout and readability

--- components/shop/productCard.tsx
+++ components/shop/productCard.tsx
@@ -16,7 +16,7 @@ export default function ProductCard({
   return (
     <div
       key={id}
-      className="group pt-4 relative flex flex-col overflow-hidden rounded-lg border hadow hover:brightness-115 hover:shadow-xl active:translate-y-1 active:shadow-md active:scale-99 duration-200 transition-all border-text bg-text z-999 max-w-[450px] w-full"
+      className="group pt-4 relative flex flex-col overflow-hidden rounded-lg border shadow hover:brightness-115 hover:shadow-xl active:translate-y-1 active:shadow-md active:scale-99 duration-200 transition-all border-text bg-text/80 z-999 max-w-[450px] w-full"
     >
       <Link href={`/shop/${category}/${id}`}>
         <Image
@@ -24,13 +24,13 @@ export default function ProductCard({
           src={images[0]?.src || "/placeholder.jpg"}
           width={500}
           height={700}
-          className="px-4 mx-auto w-[90%] bg-text object-cover sm:aspect-[3/2] sm:h-96 transition-all"
+          className="sm:px-4 mx-auto w-[90%] bg-text object-cover sm:aspect-[3/2] sm:h-96 transition-all rounded-sm "
         />
         <div className="flex flex-1 flex-col space-y-2 p-4">
-          <h3 className="text-lg font-medium text-text-secondary">{name}</h3>
-          <p className="text-base text-text-secondary">{brief}</p>
+          <h3 className="text-2xl sm:text-xl font-medium text-text-secondary">{name}</h3>
+          <p className="text-xl sm:text-lg text-text-secondary">{brief}</p>
           <div className="flex flex-1 flex-col justify-end">
-            <p className="text-base font-medium text-text-secondary">{price}</p>
+            <p className="text-lg sm:base font-medium text-text-secondary">{price}</p>
           </div>
         </div>
       </Link>
