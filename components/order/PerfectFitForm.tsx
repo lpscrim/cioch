b626b2d70429c55ba29ba1fd83d6c0dd9d5bd111
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-12-04 15:51:58 +0000

    feat: update form-type field in Perfect Fit form for consistency

--- components/order/PerfectFitForm.tsx
+++ components/order/PerfectFitForm.tsx
@@ -289,6 +289,7 @@ export default function PerfectFitForm() {
       className="max-w-4xl mx-auto space-y-12 bg-text rounded-xl shadow-lg p-8"
     >
       <input type="hidden" name="form-name" value="perfectfit" />
+      <input type="hidden" name="form-type" value="Perfect Fit" />
       <input type="hidden" name="bot-field" />
 
       <div className="border-b border-text pb-12">
