ba062e35baab6a66a0ef123c954df3ada627d856
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2026-04-16 18:47:03 +0100

    fix: update external links to use HTTPS for improved security

--- app/about/links/page.tsx
+++ app/about/links/page.tsx
@@ -15,86 +15,86 @@ export default function Links() {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-x-12 gap-y-8">
         <div>
           <Section title="Local Mountain Guides" >
-            <ExternalLink href="http://www.guidingonskye.co.uk">
+            <ExternalLink href="https://www.guidingonskye.co.uk">
               Guiding on Skye (George Yeomans)
             </ExternalLink>
-            <ExternalLink href="http://www.blavenguiding.co.uk">
+            <ExternalLink href="https://www.blavenguiding.co.uk">
               Blaven Guiding (Richard &#39;Paddy&#39; McGuire)
             </ExternalLink>
-            <ExternalLink href="http://www.skyeguides.co.uk">
+            <ExternalLink href="https://www.skyeguides.co.uk">
               Mike Lates Skye Guides
             </ExternalLink>
           </Section>
 
           <Section title="Fort William Guides">
-            <ExternalLink href="http://www.westcoast-mountainguides.co.uk">
+            <ExternalLink href="https://www.westcoast-mountainguides.co.uk">
               West Coast Mountain Guides (Alan Kimber)
             </ExternalLink>
           </Section>
 
           <Section title="Mountain Rescue Teams">
-            <ExternalLink href="http://www.cmrt.org.uk">
+            <ExternalLink href="https://www.cmrt.org.uk">
               Cairngorm Mountain Rescue Team
             </ExternalLink>
           </Section>
 
           <Section title="Equipment">
-            <ExternalLink href="http://www.campersbackpack.com">
+            <ExternalLink href="https://www.campersbackpack.com">
               Campersbackpack.com
             </ExternalLink>
           </Section>
 
           <Section title="Information About Skye &amp; Lochalsh">
-            <ExternalLink href="http://www.skye.co.uk">
+            <ExternalLink href="https://www.skye.co.uk">
               Skye &amp; Lochalsh - Official Website
             </ExternalLink>
-            <ExternalLink href="http://www.visithighlands.com">
+            <ExternalLink href="https://www.visithighlands.com">
               Visit Highlands
             </ExternalLink>
-            <ExternalLink href="http://www.uplandpaths.co.uk">
+            <ExternalLink href="https://www.uplandpaths.co.uk">
               Skye &amp; Lochalsh Footpath Initiative
             </ExternalLink>
           </Section>
         </div>
         <div>
            <Section title="Walking Holidays">
-            <ExternalLink href="http://www.scotmountain.co.uk">
+            <ExternalLink href="https://www.scotmountain.co.uk">
               Mountain Innovations
             </ExternalLink>
           </Section>
 
           <Section title="Graphic &amp; Web Design">
-            <ExternalLink href="http://www.pelican-design.com">
+            <ExternalLink href="https://www.pelican-design.com">
               Pelican Design Consultants
             </ExternalLink>
           </Section>
 
           <Section title="Virtual Reality Tour &amp; 3D Product Shots">
-            <ExternalLink href="http://www.eyeconvista.co.uk">
+            <ExternalLink href="https://www.eyeconvista.co.uk">
               eyeCon Vista Ltd.
             </ExternalLink>
           </Section>
 
           <Section title="Funding &amp; Assistance">
-            <ExternalLink href="http://www.hie.co.uk/sale">
+            <ExternalLink href="https://www.hie.co.uk/sale">
               Skye &amp; Lochalsh Enterprise
             </ExternalLink>
           </Section>
 
           <Section title="Accommodation">
-            <ExternalLink href="http://www.scottishhighlandholidays.co.uk">
+            <ExternalLink href="https://www.scottishhighlandholidays.co.uk">
               North West Scotland
             </ExternalLink>
-            <ExternalLink href="http://www.skyebasecamp.co.uk">
+            <ExternalLink href="https://www.skyebasecamp.co.uk">
               Skye Basecamp, Broadford
             </ExternalLink>
-            <ExternalLink href="http://www.taighmhuirne.co.uk/">
+            <ExternalLink href="https://www.taighmhuirne.co.uk/">
               Taigh Mhuirne, Portree
             </ExternalLink>
           </Section>
 
           <Section title="Things to do">
-            <ExternalLink href="http://www.skyemusic.co.uk">
+            <ExternalLink href="https://www.skyemusic.co.uk">
               Isle of Skye Accordion &amp; Fiddle Club
             </ExternalLink>
           </Section>
