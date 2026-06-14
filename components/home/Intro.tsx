b032a35e0ff392b2e500a7afe610db8b16a1601c
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2026-06-14 15:19:08 +0100

    fix: add unoptimized prop to Image component in Intro

--- components/home/Intro.tsx
+++ components/home/Intro.tsx
@@ -20,6 +20,7 @@ export default function Intro() {
               height={450}
               sizes="(max-width: 1024px) 400px, 450px"
               priority
+              unoptimized
             />
           </div>
           <div className="w-full lg:w-1/2 mx-auto flex flex-col items-center justify-center px-6">
